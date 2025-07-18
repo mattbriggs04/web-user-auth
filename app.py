68e17e516b0b650cd13adb8cf5baacd6ca8dd589
Matthew Briggs <matthew.briggs04@gmail.com>
2025-07-17 22:55:50 -0500

    server serves HTML

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from utils import server
-from utils import pyuser
+from utils.server import Router, AppServer
 
+app = AppServer()
+router = Router()
 
-if __name__ == "main":
-    server.start_server()
+def index():
+    with open("./templates/index.html", 'r') as page:
+        page_content = page.read()
+    return page_content
+
+if __name__ == "__main__":
+    # add routes
+    router.add_get_route("/", index)
+    router.add_get_route("/index", index)
+
+    app.start_server()
--- utils/server.py
+++ utils/server.py
@@ -1,24 +1,47 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from http import HTTPStatus
 import os
 
 class Router():
+    _instance = None
+
     def __init__(self):
         self.routes = {}
     
+    def add_get_route(self, path: str, route_fn):
+        self.routes[path] = route_fn
+
     def add_route(self, path: str, handler_fn):
         self.routes[path] = handler_fn
 
-    def retrieve_route(self, path: str):
+    def retrieve_route_fn(self, path: str):
         return self.routes.get(path)
     
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Router, cls).__new__(cls)
+        return cls._instance
+    
 class RequestHandler(BaseHTTPRequestHandler):
+    router = Router()
+
     def do_GET(self):
-        self.send_response(200)
+        route_fn = self.router.retrieve_route_fn(self.path)
+        if not route_fn:
+            self.send_error(HTTPStatus.NOT_FOUND)
+            return
+        
+        self.send_response(HTTPStatus.OK)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
-        self.wfile.write(b"")
+        self.wfile.write(route_fn().encode("utf-8"))
 
-def start_server(ip: str='localhost', port: int=8020):
-    server = HTTPServer((ip, port), RequestHandler, bind_and_activate=True)
-    print(f"Server started on http://{ip}:{port}")
-    server.serve_forever()
+class AppServer():
+    def __init__(self, ip: str='localhost', port: int=8020):
+        self.ip = ip
+        self.port = port
+
+    def start_server(self):
+        server = HTTPServer((self.ip, self.port), RequestHandler, bind_and_activate=True)
+        print(f"Server started on http://{self.ip}:{self.port}")
+        server.serve_forever()
