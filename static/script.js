ba0c15b378fd8e1f430a71b6fd92e40517ea85f4
Matthew Briggs <matthew.briggs04@gmail.com>
2025-08-18 11:40:40 -0700

    request for form submit added

---
+++ static/script.js
@@ -0,0 +1,25 @@
+const createAccountForm = document.getElementById("create-account-form");
+
+createAccountForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const data = {
+        name: document.getElementById("name-input").value,
+        email: document.getElementById("email-input").value,
+        password: document.getElementById("password-input").value,
+        confirm_password: document.getElementById("confirm-password-input").value
+    }
+
+    try {
+        const res = await fetch("/register-user", 
+            {
+                method: "POST",
+                body: JSON.stringify(data)
+            });
+        const serverResponse = await res.json()
+        console.log("Server responded with " + serverResponse)
+    }
+    catch (e) {
+        console.log(`Error: ${e}`)
+    }
+})
